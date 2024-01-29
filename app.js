af9f8ae5a3708e1ed01af2f64bccce6e4080af26
Anas-says-hi <anas.siddiquewasfound@gmail.com>
2024-01-29 12:48:23 +0600

    added styling

--- app.js
+++ app.js
@@ -23,19 +23,28 @@ const daysElm = document.querySelector(".dates");
 monthElm.innerHTML = months[date.getMonth()] + " " + date.getFullYear();
 
 const currDay = new Date(2024, MONTH - 1, 1).getDay();
-// console.log(currDay)
-// console.log(currDay)
 
 for (let i = 1; i < days + 1; i++) {
   const wrapper = document.createElement("div");
-  wrapper.className = "text-center";
+  wrapper.className = "date-elm text-center w-[30px] h-[30px] hover:bg-gray-200 flex justify-center items-center rounded-full select-none";
   wrapper.innerHTML = i;
   daysElm.append(wrapper);
 }
 
+const dateElm = document.querySelectorAll(".date-elm")
+
+dateElm.forEach(d =>{
+  d.addEventListener("click", ()=>{
+    d.classList.remove("hover:bg-gray-200")
+    d.classList.toggle("text-white")
+    d.classList.toggle("bg-emerald-600")
+    d.classList.toggle("hover:bg-emerald-700")
+  })
+})
+
 for (let i = 0; i < currDay; i++) {
   const wrapper = document.createElement("div");
-  wrapper.className = "text-center  w-[20px] h-[20px]";
+  wrapper.className = "text-center  w-[30px] h-[30px]";
   wrapper.innerHTML = "";
   daysElm.prepend(wrapper);
 }
