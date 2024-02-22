1b9f3478f6deb3217b6bc94db3005c9f7336b082
Anas-says-hi <anas.siddiquewasfound@gmail.com>
2024-02-22 19:01:24 +0600

    Build: dark-light mode

--- app.js
+++ app.js
@@ -148,6 +148,7 @@ function showCal() {
 
 function setDialogColors() {
   const colorsWrapper = document.querySelector(".dia-colors");
+  colorsWrapper.innerHTML = "";
   colors.forEach((color) => {
     colorsWrapper.innerHTML += `<div id="${color.name}" style="background-color: ${color.color};border: solid 2px ${color.dark};" class="COLOR h-[30px] aspect-square rounded-full hover:shadow-xl cursor-pointer"></div>`;
   });
@@ -209,7 +210,7 @@ addEventBtn.addEventListener("click", () => {
 });
 
 document.addEventListener("keydown", (e) => {
-  e.preventDefault();
+  // e.preventDefault();
   if (e.shiftKey && e.key === "A") {
     addModalFunctions(dialog, null);
   }
@@ -330,6 +331,36 @@ function addTask() {
   dialog.close();
 }
 
+const LIGHT = `<svg class="text-gray-700 hover:bg-gray-200 p-2 rounded-full" xmlns="http://www.w3.org/2000/svg" width="2.4em" height="2.4em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 17q-2.075 0-3.537-1.463T7 12q0-2.075 1.463-3.537T12 7q2.075 0 3.538 1.463T17 12q0 2.075-1.463 3.538T12 17M2 13q-.425 0-.712-.288T1 12q0-.425.288-.712T2 11h2q.425 0 .713.288T5 12q0 .425-.288.713T4 13zm18 0q-.425 0-.712-.288T19 12q0-.425.288-.712T20 11h2q.425 0 .713.288T23 12q0 .425-.288.713T22 13zm-8-8q-.425 0-.712-.288T11 4V2q0-.425.288-.712T12 1q.425 0 .713.288T13 2v2q0 .425-.288.713T12 5m0 18q-.425 0-.712-.288T11 22v-2q0-.425.288-.712T12 19q.425 0 .713.288T13 20v2q0 .425-.288.713T12 23M5.65 7.05L4.575 6q-.3-.275-.288-.7t.288-.725q.3-.3.725-.3t.7.3L7.05 5.65q.275.3.275.7t-.275.7q-.275.3-.687.288T5.65 7.05M18 19.425l-1.05-1.075q-.275-.3-.275-.712t.275-.688q.275-.3.688-.287t.712.287L19.425 18q.3.275.288.7t-.288.725q-.3.3-.725.3t-.7-.3M16.95 7.05q-.3-.275-.288-.687t.288-.713L18 4.575q.275-.3.7-.288t.725.288q.3.3.3.725t-.3.7L18.35 7.05q-.3.275-.7.275t-.7-.275M4.575 19.425q-.3-.3-.3-.725t.3-.7l1.075-1.05q.3-.275.712-.275t.688.275q.3.275.288.688t-.288.712L6 19.425q-.275.3-.7.288t-.725-.288"/></svg>`;
+const DARK = `<svg class="text-gray-400 hover:bg-gray-600 p-2 rounded-full" xmlns="http://www.w3.org/2000/svg" width="2.4em" height="2.4em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 21q-3.775 0-6.387-2.613T3 12q0-3.45 2.25-5.988T11 3.05q.325-.05.575.088t.4.362q.15.225.163.525t-.188.575q-.425.65-.638 1.375T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q.775 0 1.538-.225t1.362-.625q.275-.175.563-.162t.512.137q.25.125.388.375t.087.6q-.35 3.45-2.937 5.725T12 21"/></svg>`;
+let light = true;
+
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  light = false;
+}
+const lightDark = document.querySelector(".light-dark");
+
+lightDark.innerHTML = light ? LIGHT : DARK;
+
+if (!light) {
+  document.querySelector("html").className = "dark";
+} else {
+  document.querySelector("html").className = "";
+}
+
+lightDark.addEventListener("click", () => {
+  light = !light;
+  lightDark.innerHTML = light ? LIGHT : DARK;
+  if (!light) {
+    document.querySelector("html").className = "dark";
+  } else {
+    document.querySelector("html").className = "";
+  }
+});
+
 function displayTasks() {
   document.querySelectorAll(".event").forEach((e) => e.remove());
   let currElm = null;
@@ -337,23 +368,16 @@ function displayTasks() {
   EVENTS.forEach((e) => {
     if (e.name === selectedDateString && e.events.length != 0) {
       e.events.forEach((t) => {
-        document.querySelector(".time-schedule").innerHTML += `<div id="${
-          t.id
-        }" style="background-color: ${t.color}; border-left:solid 6px ${
-          t.dark
-        }; color: ${
-          t.dark
-        }" class="event m-[2px] font-semibold trans overflow-auto absolute w-[150px] min-h-[50px] h-[${
-          t.height - 2
-        }px] top-[${t.ypos}px] left-[${t.left}px] p-3 rounded-lg w-full">${
-          t.fromTime
-        } - ${t.toTime} ${t.title}</div>
+        document.querySelector(
+          ".time-schedule"
+        ).innerHTML += `<div id="${t.id}" style="background-color: ${t.color}; border-left:solid 6px ${t.dark}; color: ${t.dark}" class="event font-semibold trans absolute w-[150px] min-h-[${t.height}px] top-[${t.ypos}px] left-[${t.left}px] p-3 rounded-lg w-full">${t.fromTime} - ${t.toTime} ${t.title}</div>
         </div>`;
         currElm = document.querySelector(`#${t.id}`);
       });
       e.events[0].mod = true;
     }
   });
+  console.log(currElm);
 
   setInterval(() => {
     if (currElm) {
@@ -482,7 +506,6 @@ function setCurrDate() {
     ) {
       selectedDate.date = parseInt(d.innerText);
       d.id = "today";
-      // d.classList.remove("hover:bg-gray-200");
       d.classList.add("text-white");
       d.classList.add("bg-emerald-700");
       d.classList.add("hover:bg-emerald-800");
@@ -497,8 +520,6 @@ function selectDates() {
     d.addEventListener("click", () => {
       dateElm.forEach((d) => {
         if (d.id != "today") {
-          // d.classList.add("hover:bg-gray-200");
-          // d.classList.add("dark:hover:bg-gray-700");
           d.classList.remove("text-white");
           d.classList.remove("bg-emerald-500");
           d.classList.remove("hover:bg-emerald-600");
@@ -558,7 +579,7 @@ function eventOptions() {
 
 function showInfo() {
   document.querySelectorAll(".event").forEach((ev) => {
-    ev.addEventListener("click", () => {
+    ev.addEventListener("dblclick", () => {
       currEvID = ev.id;
 
       EVENTS.forEach((f) => {
